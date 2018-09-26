6d6bdd11a7cc4b4e7bdd7f920dd08b8d7d07238d
kisumutalent <homiemusa@gmail.com>
2018-09-26 07:46:32 +0300

    create a method that display firstname and lastname

--- employ.py
+++ employ.py
@@ -4,6 +4,9 @@ class Employee:
 		self.last = last
 		self.pay = pay
 		self.email = first + '.' + last + '@company.com'
+
+	def fullname(self):
+		return  '{} {}'.format(self.first, self.last)
           
 
 emp_1 = Employee('Corey', 'Schofar', 500000)
@@ -13,6 +16,8 @@ emp_2 = Employee('Test', 'User', 600000)
 print(emp_1.email)
 print(emp_2.email)
 
+print(emp_1.fullname())
+print (emp_2.fullname())
 
 
 
