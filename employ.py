d1b81aa0e6fc4e46e75c3340d01a57cc05fb6b2f
kisumutalent <homiemusa@gmail.com>
2018-09-26 09:29:50 +0300

    create strings to display new employee

--- employ.py
+++ employ.py
@@ -1,6 +1,6 @@
 class Employee:
 	num_of_emps = 0
-	raise_amount = 1.04
+	raise_amt = 1.04
 
 	def __init__(self, first,last,pay):
 		self.first = first
@@ -15,30 +15,31 @@ class Employee:
 		return  '{} {}'.format(self.first, self.last)
 
 	def apply_raise(self):
-		self.pay = int(self.pay * self.raise_amount)
+		self.pay = int(self.pay * self.raise_amt)
 
-
+	@classmethod
+	def set_raise_amt(cls, amount):
+		cls.raise_amt = amount
 
 emp_1 = Employee('Corey', 'Schofar', 500000)
 emp_2 = Employee('Test', 'User', 600000)
 
+emp_str_1 = 'john-Deo-70000'
+
+first, last, pay = emp_str_1.split('-')
+
+new_emp_1 = Employee(first, last, pay)
+
+print(new_emp_1.email)
+print(new_emp_1.pay)
+
+# Employee.set_raise_amt(1.05)
+# print(Employee.raise_amt)
+# print(emp_1.raise_amt)
+# print(emp_2.raise_amt)
+
 print(Employee.num_of_emps)
 
-# print(emp_1.__dict__)
-
-# # print(Employee.raise_amount)
-# # print(emp_1.raise_amount)
-# # print(emp_2.raise_amount)
-
-# # print(emp_1.pay)
-# # emp_1.apply_raise()
-# # print(emp_1.pay)
-
-# # print(emp_1.email)
-# # print(emp_2.email)
-
-# # print(emp_1.fullname())
-# # print (emp_2.fullname())
 
 
 
