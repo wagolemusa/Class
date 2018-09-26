7fe8422b93fdef6b10d11d95c6c2450a9ae60bca
kisumutalent <homiemusa@gmail.com>
2018-09-26 07:38:47 +0300

    add init method and print employee details

--- employ.py
+++ employ.py
@@ -1,18 +1,14 @@
 class Employee:
-     pass
+	def __init__(self, first,last,pay):
+		self.first = first
+		self.last = last
+		self.pay = pay
+		self.email = first + '.' + last + '@company.com'
+          
 
-emp_1 = Employee()
-emp_2 = Employee()
+emp_1 = Employee('Corey', 'Schofar', 500000)
+emp_2 = Employee('Test', 'User', 600000)
 
-emp_1.first = 'Corey'
-emp_1.last  = 'Schafer'
-emp_1.email = 'core.Schafer@company.com'
-emp_1.pay   = 50000
-
-emp_2.first = 'Test'
-emp_2.last = 'User'
-emp_2.email = 'Test.User@company.com'
-emp_2.pay   = 60000
 
 print(emp_1.email)
 print(emp_2.email)
