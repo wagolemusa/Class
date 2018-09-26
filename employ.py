70587e6ca1e6d56ab902abbe95798f97e72d27d7
kisumutalent <homiemusa@gmail.com>
2018-09-26 07:24:15 +0300

    create employee in class

---
+++ employ.py
@@ -0,0 +1,23 @@
+class Employee:
+     pass
+
+emp_1 = Employee()
+emp_2 = Employee()
+
+emp_1.first = 'Corey'
+emp_1.last  = 'Schafer'
+emp_1.email = 'core.Schafer@company.com'
+emp_1.pay   = 50000
+
+emp_2.first = 'Test'
+emp_2.last = 'User'
+emp_2.email = 'Test.User@company.com'
+emp_2.pay   = 60000
+
+print(emp_1.email)
+print(emp_2.email)
+
+
+
+
+
