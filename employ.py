a7c0de76b42d0efb05ae13f65b34e8a16416e087
kisumutalent <homiemusa@gmail.com>
2018-09-26 11:47:19 +0300

    creating methods for add, remove, print employee under Manager

--- employ.py
+++ employ.py
@@ -37,30 +37,44 @@ class Employee:
 		else:
 			print ("We will call you")
 
+
 #class developer and inheritance the Employee class
 class  Developer(Employee):
 	raise_amt = 1.10
 	def __init__(self, first, last, pay, prog_lang):
-		super().__init__(first, last, pay)
-
+		super().__init__(first, last, pay) 
 		self.prog_lang = prog_lang	
+
+# return a list of employee which maneger supervices
+class Maneger(Employee):
+	def __init__(self, first, last, pay, employees=None):
+		super().__init__(first, last, pay)
+		if employees is None:
+			self.employees = []
+		else:
+			self.employees = employees
+
+	def add_emp(self, emp):
+		if emp not in self.employees:
+			self.employees.append(emp)
+
+	def remove_emp(self, emp):
+		if emp in self.employees:
+			self.employees.remove(emp)
+
+	def print_emps(self):
+		for emp in self.employees:
+			print('---->', emp.fullname())
+
 
 dev_1 = Developer('Corey', 'Schofar', 500000, 'python')
 dev_2 = Developer('Test', 'User', 600000, 'java')
 
+mgr_1 = Maneger('Sue', 'Smith', 900000)
 
-
-print(dev_1.email)
-print(dev_1.prog_lang)
-
-
-# print(help(Developer))
+print (mgr_1.email)
 # print(dev_1.email)
-# print(dev_2.email)
-
-# print(dev_1.pay)
-# dev_1.apply_raise()
-# print(dev_1.pay)
+# print(dev_1.prog_lang)
 
 
 
