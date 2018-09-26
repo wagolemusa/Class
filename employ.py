e88d28e8331365594f7e6cecd44075250f2e385e
kisumutalent <homiemusa@gmail.com>
2018-09-26 18:40:24 +0300

    implament repr and str methods

--- employ.py
+++ employ.py
@@ -1,4 +1,7 @@
 class Employee:
+
+	raise_amt = 1.04
+
 	def __init__(self, first,last,pay):
 		self.first = first
 		self.last = last
@@ -8,18 +11,26 @@ class Employee:
 		# method to dispaly fullname
 	def fullname(self):
 		return  '{} {}'.format(self.first, self.last)
+
+	def apply_raise(self):
+		self.pay = int(self.pay * self.raise_amt)
+
+	#it was design to be seen by Developers
+	def __repr__(self):
+		return "Employee('{}', '{}', {})".format(self.first, self.last, self.pay)
+
+	#it was desigin to be seen by users, Which means it displays
+	def __str__(self):
+		return '{} - {}'.format(self.fullname(), self.email)
           
 
 emp_1 = Employee('Corey', 'Schofar', 500000)
 emp_2 = Employee('Test', 'User', 600000)
 
+#print(emp_1)
 
-print(emp_1.email)
-print(emp_2.email)
-
-print(emp_1.fullname())
-print (emp_2.fullname())
-
+print(repr(emp_1))
+print(str(emp_2))
 
 
 
