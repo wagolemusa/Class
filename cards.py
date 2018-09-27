d277d21979b2670e28ac9b863121c30307f1dfd6
kisumutalent <homiemusa@gmail.com>
2018-09-27 22:16:27 +0300

    create function to append cards in list & show methods

--- cards.py
+++ cards.py
@@ -1,12 +1,32 @@
-class Card(object):
+class Cards(object):
 	def __init__(self, suit, val):
 		self.suit = suit
 		self.value = val
 
+	#display cards
+	def show(self):
+		print '{} of {}'.format(self.value, self.suit)
+
+
+class Deck(object):
+	def __init__(self):
+		self.cards = []
+		self.build()
+
+	def	build(self):
+		for s in ["kamuli", "supedi", "mutima", "Dai"]:
+			for v in range(1, 14):
+				self.cards.append(Cards(s, v))
 
 	def show(self):
-		print "{} of {}".format(self.value, self.suit)
+		for c in self.cards:
+			c.show()
+		
 
-card = Card("club", 6)
 
-card.show()
+# card = Card("club", 6)
+
+# card.show()
+
+me = Deck()
+me.show()
